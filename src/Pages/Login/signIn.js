ce6613b2cf2b81f52aeef33b4e0091106ae2d5d2
adesug <adesugiantoro89@gmail.com>
2021-09-24 22:28:02 +0700

    replace bg signin

--- src/Pages/Login/signIn.js
+++ src/Pages/Login/signIn.js
@@ -17,12 +17,12 @@ export default function SignIn() {
                 </p>
             
 
-                <img className="object-cover w-full h-screen hidden md:block" src="https://res.cloudinary.com/desug/image/upload/v1630960002/login_fea4xn.jpg" alt="gambar signin" />
+                <img className="object-cover w-full h-screen hidden md:block" src="https://res.cloudinary.com/desug/image/upload/v1632495038/Plugin/photo1632494998_whbdpz.jpg" alt="gambar signin" />
             </div>
 
             <div className="md:w-1/2 flex flex-col justify-center ">
                 
-            <img className="object-cover w-screen h-screen block lg:hidden md:hidden" src="https://res.cloudinary.com/desug/image/upload/v1630960002/login_fea4xn.jpg" alt="gambar signin" />
+            <img className="object-cover w-screen h-screen block lg:hidden md:hidden" src="https://res.cloudinary.com/desug/image/upload/v1632495038/Plugin/photo1632494998_whbdpz.jpg" alt="gambar signin" />
                 <div className="flex flex-col md:justify-start my-auto pt-8 md:pt-0 px-7 md:px-10 lg:px-32 absolute">
 
                     <p className="text-3xl mr-auto text-blue-400 font-semibold">
